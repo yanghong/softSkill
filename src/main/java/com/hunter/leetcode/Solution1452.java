06d76676305a7d9dccc5e6d3f84e1ab0561348bc
hunter.yang <hunter.yang@beibei.com>
2020-10-27 09:25:49 +0800

    leetCode 1452

---
+++ src/main/java/com/hunter/leetcode/Solution1452.java
@@ -0,0 +1,36 @@
+package com.hunter.leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author hunter.yang
+ * @version 1.0
+ * @description null
+ * @date 2020/10/27 8:18
+ */
+public class Solution1452 {
+
+    public static List<Integer> peopleIndexes(List<List<String>> favoriteCompanies) {
+
+        List<Integer> result = new ArrayList<>();
+
+
+
+        return result;
+
+    }
+
+    private static boolean isSubSet(List<String> companiesOne, List<String> companiesTwo) {
+        Collections.sort(companiesOne);
+        Collections.sort(companiesTwo);
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
