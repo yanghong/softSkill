63b7d250ccb054688bb61488bb5be3578eed22d5
hunter.yang <hunter.yang@beibei.com>
2020-12-16 15:41:00 +0800

    add 降级切面

---
+++ src/main/java/com/hunter/spring/intercept/DegradeAnnotation.java
@@ -0,0 +1,30 @@
+package com.hunter.spring.intercept;
+
+import java.lang.annotation.*;
+
+/**
+ * @author hunter.yang
+ * @version 1.0
+ * @description 降级切面
+ * @date 2020/12/16 11:25
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface DegradeAnnotation {
+
+    /**
+     * 降级等级 等级越高越重要
+     *
+     * @return
+     */
+    int level();
+
+    /**
+     * 方法别名，用于配置搭配hconf中的，一般命名规则可以是className_methodName 如果存在重载方法，则在后面加编号即可
+     * 原则：绝不能重复，一旦有重复，可能导致降了不该降级的接口
+     *
+     * @return
+     */
+    String alias();
+}
---
+++ src/main/java/com/hunter/spring/intercept/DegradeAspect.java
@@ -0,0 +1,67 @@
+package com.hunter.spring.intercept;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+/**
+ * @author hunter.yang
+ * @version 1.0
+ * @description DegradeAspect
+ * @date 2020/12/16 11:24
+ */
+@Component
+@Aspect
+@Slf4j
+public class DegradeAspect {
+
+    @Around(value = "@annotation(com.hunter.spring.intercept.DegradeAnnotation)")
+    public Object process(ProceedingJoinPoint joinPoint) {
+
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        DegradeAnnotation annotation = method.getAnnotation(DegradeAnnotation.class);
+
+        int level = annotation.level();
+//        if (level == DegradeLevelConstants.LEVEL_1) {
+//            //如果等级为1，那么需要先去取 DEGRADE_LEVEL1_SWITCH
+//            boolean level1Switch = BconfClient.isSwitchOn(BconfConstants.DEGRADE_LEVEL1_SWITCH, false);
+//            if (level1Switch) {
+//                return null;
+//            }
+//        }
+
+        //如果hconf中配置的level>=方法注解中的level，则直接降级
+//        String confValue = BconfClient.getConfValue(BconfConstants.DEGRADE_LEVEL_CONF, "-1");
+//        if (StringUtils.isNumeric(confValue)) {
+//            int confLevel = Integer.parseInt(confValue);
+//            if (confLevel >= level) {
+//                return null;
+//            }
+//        }
+        //否则，根据方法别名去配置中获取是否存在此方法的单点降级，如果存在则直接降级
+        String alias = annotation.alias();
+//        String aliasConf = BconfClient.getConfValue(BconfConstants.DEGRADE_METHOD_ALIAS_CONF);
+//        if (StringUtils.isNotBlank(aliasConf) && StringUtils.isNotBlank(alias)) {
+//            String[] aliasArray = aliasConf.split(",");
+//            boolean contains = ArrayUtils.contains(aliasArray, alias);
+//            if (contains) {
+//                return null;
+//            }
+//        }
+
+        try {
+            return joinPoint.proceed();
+        } catch (Throwable throwable) {
+            log.info("调用外部方法出错，方法名{}", method.getName(), throwable);
+            return null;
+        }
+
+    }
+
+}
