6bc3aa9d68357ade60ee661704eba4a807957009
hunteryoung <921348203@qq.com>
2021-03-23 23:42:33 +0800

    dp code learn

--- controller/src/main/java/com/hunter/leetcode/dp/Slices.java
+++ controller/src/main/java/com/hunter/leetcode/dp/Slices.java
@@ -45,6 +45,7 @@ public class Slices {
 
     public static void main(String[] args) {
         int[] A = {1,2,3,4};
+        System.out.println(4 >> 1);
         System.out.println(numberOfArithmeticSlices(A));
     }
 
